afe6bc5ad759e29607e4d1199f99c6637ff42585
Dan Goodman <dan@danthegoodman.com>
2018-04-15 17:31:18 -0400

    Changed file size

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
 const multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+    fileSize: 5 * 1024 * 1024 * 1024 // no larger than 5gb
   }
 });
 
